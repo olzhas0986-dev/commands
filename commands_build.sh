b62a67109c40b923f8b19b7cc6c9f75ecfa83837
olzhas0986-dev <olzhaskdyr0986@gmail.com>
2025-04-30 12:30:20 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -15,7 +15,7 @@ rm -rf device/xiaomi/munch
 rm -rf device/xiaomi/sm8250-common
 #rm -rf kernel/xiaomi/sm8250; \
 #rm -rf vendor/xiaomi/munch
-#rm -rf vendor/xiaomi/sm8250-common
+rm -rf vendor/xiaomi/sm8250-common
 #rm -rf hardware/xiaomi
 #rm -rf vendor/xiaomi/munch-firmware; \
 #rm -rf vendor/xiaomi/miuicamera 
@@ -26,7 +26,7 @@ git clone https://github.com/olzhas0986-dev/device_xiaomi_munch device/xiaomi/mu
 git clone https://github.com/olzhas0986-dev/device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
 #git clone https://github.com/rik-x777/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
 #git clone https://codeberg.org/rik/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
-#git clone https://codeberg.org/rik/vendor_xiaomi_sm8250-common.git vendor/xiaomi/sm8250-common; \
+git clone https://github.com/olzhas0986-dev/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
 #git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
 #git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 #git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings; \
