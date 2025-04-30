aed3c5ef0d71bb52b89b2a1e6ce54b72177abe58
olzhas0986-dev <olzhaskdyr0986@gmail.com>
2025-04-30 19:49:04 +0500

    Edit hardware

--- commands_build.sh
+++ commands_build.sh
@@ -16,7 +16,7 @@ rm -rf device/xiaomi/sm8250-common
 #rm -rf kernel/xiaomi/sm8250; \
 #rm -rf vendor/xiaomi/munch
 rm -rf vendor/xiaomi/sm8250-common
-#rm -rf hardware/xiaomi
+rm -rf hardware/xiaomi
 #rm -rf vendor/xiaomi/munch-firmware; \
 #rm -rf vendor/xiaomi/miuicamera 
 #rm -rf packages/resources/devicesettings
@@ -27,7 +27,7 @@ git clone https://github.com/olzhas0986-dev/device_xiaomi_sm8250-common device/x
 #git clone https://github.com/rik-x777/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
 #git clone https://codeberg.org/rik/vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 git clone https://github.com/olzhas0986-dev/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
-#git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
+git clone https://github.com/olzhas0986-dev/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi; \
 #git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 #git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings; \
 #git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX; \
