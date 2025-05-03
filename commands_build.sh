3b00be1d24aceff12c3151c4be5ad0517d16fcb7
olzhas0986-dev <olzhaskdyr0986@gmail.com>
2025-05-03 09:35:44 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
 # Removing local_manifests
-rm -rf .repo/local_manifests; \
+#rm -rf .repo/local_manifests; \
 
 # Init
-repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs; \
+#repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs; \
 
 # repo sync
-/opt/crave/resync.sh; \
+#/opt/crave/resync.sh; \
 
 # Removing old repos
 rm -rf out/target/product/munch
 rm -rf device/xiaomi/munch
-rm -rf device/xiaomi/sm8250-common
-rm -rf kernel/xiaomi/sm8250; \
-rm -rf vendor/xiaomi/munch
-rm -rf vendor/xiaomi/sm8250-common
-rm -rf hardware/xiaomi
-rm -rf vendor/xiaomi/munch-firmware; \
-rm -rf vendor/xiaomi/miuicamera 
-rm -rf packages/resources/devicesettings
-rm -rf packages/apps/ViPER4AndroidFX; \
+#rm -rf device/xiaomi/sm8250-common
+#rm -rf kernel/xiaomi/sm8250; \
+#rm -rf vendor/xiaomi/munch
+#rm -rf vendor/xiaomi/sm8250-common
+#rm -rf hardware/xiaomi
+#rm -rf vendor/xiaomi/munch-firmware; \
+#rm -rf vendor/xiaomi/miuicamera 
+#rm -rf packages/resources/devicesettings
+#rm -rf packages/apps/ViPER4AndroidFX; \
 
 # Cloning repos
-git clone https://github.com/olzhas0986-dev/android_device_xiaomi_munch.git device/xiaomi/munch; \
+git clone https://github.com/olzhas0986-dev/android_device_xiaomi_munch.git -b voltage device/xiaomi/munch; \
 
-git clone https://github.com/Pureline-Kernel/pure_xiaomi_sm8250.git kernel/xiaomi/munch; \
+#git clone https://github.com/Pureline-Kernel/pure_xiaomi_sm8250.git kernel/xiaomi/munch; \
 
-git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
+#git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 
-git clone https://github.com/munch-devs/android_hardware_xiaomi.git hardware/xiaomi; \
+#git clone https://github.com/munch-devs/android_hardware_xiaomi.git hardware/xiaomi; \
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
+#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings; \
+#git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings; \
 ########
 
 # Building
