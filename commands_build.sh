79227afbe0b9a4e1265f58798d61aebde0a1b3ec
olzhas0986-dev <olzhaskdyr0986@gmail.com>
2025-05-03 11:14:02 +0500

    Some rework

--- commands_build.sh
+++ commands_build.sh
@@ -3,13 +3,13 @@
 # Removing local_manifests
 #rm -rf .repo/local_manifests; \
 
-# Init
+# Init ROM
 #repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs; \
 
 # repo sync
 #/opt/crave/resync.sh; \
 
-# Removing old repos
+# Removing old device specific repos
 rm -rf out/target/product/munch
 rm -rf device/xiaomi/munch
 #rm -rf device/xiaomi/sm8250-common
@@ -21,8 +21,9 @@ rm -rf device/xiaomi/munch
 #rm -rf vendor/xiaomi/miuicamera 
 #rm -rf packages/resources/devicesettings
 #rm -rf packages/apps/ViPER4AndroidFX; \
+######### End
 
-# Cloning repos
+# Cloning new device specific repos
 git clone https://github.com/olzhas0986-dev/android_device_xiaomi_munch.git -b voltage device/xiaomi/munch; \
 
 #git clone https://github.com/Pureline-Kernel/pure_xiaomi_sm8250.git kernel/xiaomi/munch; \
@@ -34,8 +35,9 @@ git clone https://github.com/olzhas0986-dev/android_device_xiaomi_munch.git -b v
 #git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 
 #git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings; \
-########
+######## End
 
-# Building
+# Building: VoltageOS
 . build/envsetup.sh; \
 brunch munch
+######## End
