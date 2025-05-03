f839e6fd201add8ece3f5237a40db3c4804e2a2a
olzhas0986-dev <olzhaskdyr0986@gmail.com>
2025-05-03 12:37:10 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
 # Removing local_manifests
-#rm -rf .repo/local_manifests; \
+rm -rf .repo/local_manifests; \
 
 # Init ROM
-#repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs; \
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; \
 
 # repo sync
-#/opt/crave/resync.sh; \
+/opt/crave/resync.sh; \
 
 # Removing old device specific repos
 rm -rf out/target/product/munch
 rm -rf device/xiaomi/munch
-#rm -rf device/xiaomi/sm8250-common
-#rm -rf kernel/xiaomi/sm8250; \
-#rm -rf vendor/xiaomi/munch
-#rm -rf vendor/xiaomi/sm8250-common
-#rm -rf hardware/xiaomi
-#rm -rf vendor/xiaomi/munch-firmware; \
-#rm -rf vendor/xiaomi/miuicamera 
-#rm -rf packages/resources/devicesettings
-#rm -rf packages/apps/ViPER4AndroidFX; \
+rm -rf device/xiaomi/sm8250-common
+rm -rf kernel/xiaomi/sm8250; \
+rm -rf vendor/xiaomi/munch
+rm -rf vendor/xiaomi/sm8250-common
+rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi/munch-firmware; \
+rm -rf vendor/xiaomi/miuicamera 
+rm -rf packages/resources/devicesettings
+rm -rf packages/apps/ViPER4AndroidFX; \
 ######### End
 
 # Cloning new device specific repos
-git clone https://github.com/olzhas0986-dev/android_device_xiaomi_munch.git -b voltage device/xiaomi/munch; \
+git clone https://github.com/olzhas0986-dev/android_device_xiaomi_munch.git -b infinity device/xiaomi/munch; \
 
-#git clone https://github.com/Pureline-Kernel/pure_xiaomi_sm8250.git kernel/xiaomi/munch; \
+git clone https://github.com/Pureline-Kernel/pure_xiaomi_sm8250.git kernel/xiaomi/munch; \
 
-#git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
+git clone https://github.com/munch-devs/android_vendor_xiaomi_munch.git vendor/xiaomi/munch; \
 
-#git clone https://github.com/munch-devs/android_hardware_xiaomi.git hardware/xiaomi; \
+git clone https://github.com/munch-devs/android_hardware_xiaomi.git hardware/xiaomi; \
 
-#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b vic vendor/xiaomi/miuicamera; \
 
-#git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings; \
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings; \
 ######## End
 
 # Useless stuff
@@ -43,7 +43,7 @@ export BUILD_HOSTNAME=crave
 export TZ=Asia/Atyrau
 ######### End
 
-# Building: VoltageOS
+# Building: Infinity-X
 . build/envsetup.sh; \
-brunch munch
+lunch infinity_munch-user
 ######### End
