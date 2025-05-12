3d809fed0c8746a93621c21281f2b8956939374c
olzhas0986-dev <olzhaskdyr0986@gmail.com>
2025-05-12 15:11:32 +0500

    Update commands_build.sh

--- commands_build.sh
+++ commands_build.sh
@@ -2,8 +2,9 @@
 
 rm -rf .repo/local_manifests; \
 
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; \
+#repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; \
 #repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs; \
+repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs; \
 
 /opt/crave/resync.sh; \
 
@@ -21,7 +22,7 @@ git clone https://gitlab.com/rik-x777/vendor_xiaomi_sm8250-common vendor/xiaomi/
 
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
-git clone https://github.com/olzhas0986-dev/pure_xiaomi_sm8250 kernel/xiaomi/sm8250; \
+git clone https://github.com/rik-x777/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250; \
 
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi; \
 
@@ -32,8 +33,9 @@ git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/app
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 
 export TZ=Asia/Atyrau
+export BUILD_USERNAME=olzhas0986
 
-# Building: AxionAOSP
+# Building
 . build/envsetup.sh; \
-lunch infinity_munch-userdebug; \
-mka bacon #mmmm bacon 
+riseup munch userdebug
+rise b
